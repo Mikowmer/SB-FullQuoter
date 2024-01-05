4601394b65bdf1b04da3a74de30e8dd31b9fac5b
Mikowmer <mikowmer@gmail.com>
2024-01-05 22:41:12 +1100

    Update ReportLikeGapper.user.js
    added version for updating

--- ReportLikeGapper.user.js
+++ ReportLikeGapper.user.js
@@ -3,15 +3,17 @@
 // @namespace   Spacebattles
 // @description Adds a gap between the Report button and the Like button for very narrow screens and large text sizes
 // @match       https://forums.spacebattles.com/threads/*
-// @version     0.1
+// @version     0.2
 // @icon        https://forums.spacebattles.com/data/svg/2/1/1704166523/2022_favicon_192x192.png
 // @author      Mikowmer
 // @grant       GM.registerMenuCommand
-// @downloadURL https://github.com/Mikowmer/SB-FullQuoter/raw/main/QuoteQuoter.user.js
-// @updateURL   https://github.com/Mikowmer/SB-FullQuoter/raw/main/QuoteQuoter.user.js
+// @downloadURL https://github.com/Mikowmer/SB-FullQuoter/raw/main/ReportLikeGapper.user.js
+// @updateURL   https://github.com/Mikowmer/SB-FullQuoter/raw/main/ReportLikeGapper.user.js
 // ==/UserScript==
 
 function postFinder(){
+  console.log("ReportLikeGapper v0.2")
+  
   let posts_footer = document.getElementsByTagName("footer");
    for (let i = 0; i < posts_footer.length; i += 1){
      let gap = document.createElement("div");
