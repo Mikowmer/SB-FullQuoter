dbd1f25fc75edd239a104c224e0046e605c7547c
Mikowmer <mikowmer@gmail.com>
2024-01-05 22:45:55 +1100

    Update ReportLikeGapper.user.js

--- ReportLikeGapper.user.js
+++ ReportLikeGapper.user.js
@@ -3,7 +3,7 @@
 // @namespace   Spacebattles
 // @description Adds a gap between the Report button and the Like button for very narrow screens and large text sizes
 // @match       https://forums.spacebattles.com/threads/*
-// @version     0.2
+// @version     0.3
 // @icon        https://forums.spacebattles.com/data/svg/2/1/1704166523/2022_favicon_192x192.png
 // @author      Mikowmer
 // @downloadURL https://github.com/Mikowmer/SB-FullQuoter/raw/main/ReportLikeGapper.user.js
@@ -11,7 +11,7 @@
 // ==/UserScript==
 
 function postFinder(){
-  console.log("ReportLikeGapper v0.2")
+  console.log("ReportLikeGapper v0.3")
   
   let posts_footer = document.getElementsByTagName("footer");
    for (let i = 0; i < posts_footer.length; i += 1){
